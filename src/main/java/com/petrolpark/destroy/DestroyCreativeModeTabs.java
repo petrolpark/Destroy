7486c9e78ecea3cc8f481912ae85216e170745e2
petrolpark <p3tr0lp4rk@gmail.com>
2022-08-23 20:53:39 +0100

    Added all item models and Creative Mode Tab

---
+++ src/main/java/com/petrolpark/destroy/DestroyCreativeModeTabs.java
@@ -0,0 +1,13 @@
+package com.petrolpark.destroy;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+
+public class DestroyCreativeModeTabs {
+    public static final CreativeModeTab TAB_DESTROY = new CreativeModeTab("destroy_tab") {
+        @Override
+        public ItemStack makeIcon() {
+            return new ItemStack(DestroyItems.ABS.get());
+        }
+    };
+}
--- src/main/java/com/petrolpark/destroy/DestroyItems.java
+++ src/main/java/com/petrolpark/destroy/DestroyItems.java
@@ -1,7 +1,6 @@
 package com.petrolpark.destroy;
 
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -11,8 +10,50 @@ public class DestroyItems {
     public static final DeferredRegister<Item> ITEMS =
         DeferredRegister.create(ForgeRegistries.ITEMS, Destroy.MOD_ID);
 
+    // PLASTICS
+    public static final RegistryObject<Item> POLYETHENE_TEREPHTHALATE = ITEMS.register("polyethene_terephthalate",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYVINYL_CHLORIDE = ITEMS.register("polyvinyl_chloride",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYETHENE = ITEMS.register("polyethene",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYPROPENE = ITEMS.register("polypropene",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYSTYRENE = ITEMS.register("polystyrene",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> ABS = ITEMS.register("abs",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYTETRAFLUOROETHENE = ITEMS.register("polytetrafluoroethene",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> NYLON = ITEMS.register("nylon",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> POLYSTYRENE_BUTADIENE = ITEMS.register("polystyrene_butadiene",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    public static final RegistryObject<Item> RUBBER = ITEMS.register("rubber",
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
+    );
+
+    // RAW METALS
     public static final RegistryObject<Item> PURE_GOLD_DUST = ITEMS.register("pure_gold_dust",
-        () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC))
+        () -> new Item(new Item.Properties().tab(DestroyCreativeModeTabs.TAB_DESTROY))
     );
 
     public static void register(IEventBus eventBus) {
