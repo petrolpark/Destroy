8ece4cda5fee25288e344bdb18f57580c5cde68e
petrolpark <p3tr0lp4rk@gmail.com>
2023-07-12 16:56:53 +0100

    🫗 Basin overfilled alert

--- src/main/java/com/petrolpark/destroy/mixin/BasinBlockEntityMixin.java
+++ src/main/java/com/petrolpark/destroy/mixin/BasinBlockEntityMixin.java
@@ -3,12 +3,10 @@ package com.petrolpark.destroy.mixin;
 import java.util.List;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.petrolpark.destroy.block.entity.behaviour.BasinTooFullBehaviour;
 import com.petrolpark.destroy.util.DestroyLang;
+import com.simibubi.create.content.equipment.goggles.IHaveHoveringInformation;
 import com.simibubi.create.content.processing.basin.BasinBlockEntity;
 import com.simibubi.create.foundation.item.TooltipHelper;
 
@@ -16,15 +14,17 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 
 @Mixin(BasinBlockEntity.class)
-public class BasinBlockEntityMixin {
+public class BasinBlockEntityMixin implements IHaveHoveringInformation {
     
-    @Inject(method = "addToGoggleTooltip", at = @At(value = "RETURN"), cancellable = true)
-    public void inAddToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking, CallbackInfoReturnable<Boolean> ci) {
+    public boolean addToTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
         if (((BasinBlockEntity)(Object)this).getBehaviour(BasinTooFullBehaviour.TYPE).tooFullToReact) {
             DestroyLang.translate("tooltip.basin.too_full.title").style(ChatFormatting.GOLD).forGoggles(tooltip);
             TooltipHelper.cutStringTextComponent(DestroyLang.translate("tooltip.basin.too_full").string(), TooltipHelper.Palette.GRAY_AND_WHITE).forEach(component -> {
                 DestroyLang.builder().add(component.copy()).forGoggles(tooltip);
             });
+            return true;
         };
+        tooltip.add(Component.literal(""));
+        return false;
     };
 };
